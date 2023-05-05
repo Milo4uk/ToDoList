c7b70314d8edbbcfa0af1ead668748cae63e8a86
Milo4uk <wlir0tn@gmail.com>
2023-05-05 20:22:11 +0300

    initial commit

---
+++ ToDoList/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Mila ✨ on 30.04.2023.
+//
+
+import Foundation
---
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ToDoList
+//
+//  Created by Mila ✨ on 30.04.2023.
+//
+
+import Foundation
